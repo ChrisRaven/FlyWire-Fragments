46bf0618fa791d041df49d120caac8c8bbdfc29c
KrzysztofKruk <kr.kruk@gmail.com>
2022-08-13 19:09:00 +0200

    Version bump

--- Fragments.user.js
+++ Fragments.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fragments
 // @namespace    KrzysztofKruk-FlyWire
-// @version      0.1
+// @version      0.1.1
 // @description  Keeps history of removed fragments
 // @author       Krzysztof Kruk
 // @match        https://ngl.flywire.ai/*
